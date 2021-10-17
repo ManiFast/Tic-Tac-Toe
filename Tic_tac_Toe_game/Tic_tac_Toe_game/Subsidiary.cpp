44e47ea1fc8f67d2dd3cc56b3251dbc2926be872
Stas-inside <karakashstas5@gmail.com>
2021-10-17 09:32:54 +0500

    add v7

---
+++ Tic_tac_Toe_game/Tic_tac_Toe_game/Subsidiary.cpp
@@ -0,0 +1,13 @@
+#include "Subsidiary.h"
+
+void todo()
+{
+  Beep(300, 300);
+}
+
+int randNum()
+{
+  srand(time(NULL));
+
+  return rand() % 2 + 1;
+}
---
+++ Tic_tac_Toe_game/Tic_tac_Toe_game/Subsidiary.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef SUBSIDIARY_H
+#define SUBSIDIARY_H
+
+#include <iostream>
+#include <Windows.h>
+#include <ctime>
+
+void todo();
+
+int randNum();
+
+#endif // SOUNDS_H
